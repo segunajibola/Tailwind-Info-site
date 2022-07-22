698e7ce0cd335a796f613925853f37e29a72ee52
Segun Ajibola <ajbl.segun@gmail.com>
2022-07-22 02:34:21 +0100

    tailwindFact

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Main() {
     return (
         <main>
-            <h1 className="main--title">Fun facts about React</h1>
+            <h1 className="main--title">Fun facts about Tailwind</h1>
             <ul className="main--facts">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
